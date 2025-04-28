acff084c4a16997b56222080f479e57cb0b9a137
ayeshag7 <ayeshaiqbal2592@gmail.com>
2025-04-28 14:10:51 +0500

    Added horizontal scroll animation to the stack div

--- src/pages/Home/components/StackDiv.js
+++ src/pages/Home/components/StackDiv.js
@@ -3,12 +3,14 @@ import git from "../../../assets/git.png";
 import redux from "../../../assets/redux.png";
 import docker from "../../../assets/docker.png";
 import chatgpt from "../../../assets/chatgpt.png";
+import firebase from "../../../assets/firebase.svg"
 
 export const StackDiv = () => {
   return (
     <div className="stackDimensions bg-projectshadow">
-        <div className="flex justify-around items-center flex-wrap">
+        <div className="flex justify-around items-center flex-wrap animate-scroll">
           <img className="h-20 w-20 md:h-28 md:w-28" src={mongo} alt="mongo db" />
+          <img className="h-10 w-10 md:h-16 md:w-16" src={firebase} alt="firebase" />
           <img className="h-12 w-12 md:h-20 md:w-20" src={git} alt="git" />
           <img className="h-6 w-6 md:h-12 md:w-12" src={redux} alt="redux" />
           <img className="h-10 w-10 md:h-16 md:w-16" src={docker} alt="docker" />
